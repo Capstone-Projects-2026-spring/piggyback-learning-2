66956c349717d51dd3134158186334a48e315256
adam-marx-temple <amarx097@gmail.com>
2026-02-28 09:13:10 -0500

    very basic unit tests for videos/urls

--- videos/tests.py
+++ videos/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+
+# Very basic, verifies urls are active / return 200 on a GET
+class urlsTests(TestCase):
+    def test_kids_videos_endpoint(self):
+        url = reverse('kids-videos')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_video_list_endpoint(self):
+        url = reverse('video-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
