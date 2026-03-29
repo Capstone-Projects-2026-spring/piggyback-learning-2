a42d976a6bf90be72942e2f14fc4b5c48155846e
ayushgupta0010 <ayushgupta0010@gmail.com>
2026-03-29 12:07:38 -0400

    Remove all junk

--- quizgen/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class QuizgenConfig(AppConfig):
-    name = 'quizgen'
--- user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import VerifyPasswordAPIView
-
-urlpatterns = [
-    path('verify-password', VerifyPasswordAPIView.as_view(), name='verify-password'),
-]
