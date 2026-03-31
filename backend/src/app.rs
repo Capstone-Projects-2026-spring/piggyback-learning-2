857adbc52031718a11bbbc1b93424d1448e764e5
ayushgupta0010 <ayushgupta0010@gmail.com>
2026-03-31 10:37:48 -0400

    Add APIs for questions

--- backend/src/app.rs
+++ backend/src/app.rs
@@ -49,8 +49,9 @@ impl Hooks for App {
             .prefix("/api")
             .add_route(controllers::auth::routes())
             .add_route(controllers::frames::routes())
-            .add_route(controllers::openai::routes())
             .add_route(controllers::kids::routes())
+            .add_route(controllers::openai::routes())
+            .add_route(controllers::questions::routes())
             .add_route(controllers::tags::routes())
             .add_route(controllers::videos::routes())
     }
--- backend/src/controllers/mod.rs
+++ backend/src/controllers/mod.rs
@@ -2,5 +2,6 @@ pub mod auth;
 pub mod frames;
 pub mod kids;
 pub mod openai;
+pub mod questions;
 pub mod tags;
 pub mod videos;
