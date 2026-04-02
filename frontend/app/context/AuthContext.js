96eb971359b582d6be87f6a530d88ed73771b3c9
ayushgupta0010 <ayushgupta0010@gmail.com>
2026-04-01 22:28:57 -0400

    Add and export role and account in AuthContext

--- frontend/app/context/AuthContext.js
+++ frontend/app/context/AuthContext.js
@@ -8,12 +8,22 @@ export function AuthProvider({ children }) {
   const [token, setToken] = useState(
     typeof window !== "undefined" ? localStorage.getItem("token") : null,
   );
+  const [role, setRole] = useState(
+    typeof window !== "undefined" ? localStorage.getItem("role") : null,
+  );
+  const [account, setAccount] = useState(
+    typeof window !== "undefined"
+      ? JSON.parse(localStorage.getItem("account"))
+      : null,
+  );
 
   const login = (newToken, role, account) => {
     localStorage.setItem("token", newToken);
     localStorage.setItem("role", role);
     localStorage.setItem("account", JSON.stringify(account));
     setToken(newToken);
+    setRole(role);
+    setAccount(account);
   };
 
   const logout = () => {
@@ -21,10 +31,12 @@ export function AuthProvider({ children }) {
     localStorage.removeItem("role");
     localStorage.removeItem("account");
     setToken(null);
+    setRole(null);
+    setAccount(null);
   };
 
   return (
-    <AuthContext.Provider value={{ token, login, logout }}>
+    <AuthContext.Provider value={{ token, role, account, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
