13d849753df20085fc70efae7959e4184c1b0bba
ayushgupta0010 <ayushgupta0010@gmail.com>
2026-04-05 02:56:54 -0400

    Add VOSK_DIR as env variable

--- backend/src/utils/voice/state.rs
+++ backend/src/utils/voice/state.rs
@@ -1,6 +1,8 @@
 use once_cell::sync::Lazy;
+use std::env;
 use vosk::Model;
 
 pub static VOSK_MODEL: Lazy<Model> = Lazy::new(|| {
-    Model::new("vosk/vosk-model-small-en-us-0.15").expect("Failed to load Vosk model")
+    Model::new(env::var("VOSK_DIR").expect("VOSK_DIR must be set."))
+        .expect("Failed to load Vosk model")
 });
