02e8bcc872c82abfdca1f8011a46100ecdfbca16
ayushgupta0010 <ayushgupta0010@gmail.com>
2026-04-04 23:12:11 -0400

    Add tests for kids

--- backend/tests/requests/answers.rs
+++ backend/tests/requests/answers.rs
@@ -7,7 +7,6 @@ use serial_test::serial;
 async fn can_get_answers() {
     request::<App, _, _>(|request, _ctx| async move {
         let response = request.get("/api/answers/1/l2FQ8ni1MfM").await;
-
         assert_eq!(response.status_code(), 200);
     })
     .await;
