465f796797462fb31e5f685dbc716a145ff6eff1
ayushgupta0010 <ayushgupta0010@gmail.com>
2026-04-06 17:42:35 -0400

    Remove junk

--- quizgen/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-
-from .consumers import QuestionsConsumer
-
-websocket_urlpatterns = [
-    re_path(r'^ws/questions/(?P<video_id>[^/]+)/?$', QuestionsConsumer.as_asgi()),
-]
--- videos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import KidsVideosAPIView, VideoListAPIView
-
-urlpatterns = [
-    path('kids_videos', KidsVideosAPIView.as_view(), name='kids-videos'),
-    path('videos-list', VideoListAPIView.as_view(), name='video-list'),
-]
