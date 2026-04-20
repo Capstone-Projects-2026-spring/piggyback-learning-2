22353089c65f9f0151c6cdd9125cb069e14931cf
ayushgupta0010 <ayushgupta0010@gmail.com>
2026-04-20 00:30:25 -0400

    Invoke voice commmand in tauri handler

--- desktop/src-tauri/src/lib.rs
+++ desktop/src-tauri/src/lib.rs
@@ -1,14 +1,24 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+mod commands;
+mod utils;
+
+use tauri::Manager;
+use utils::voice::state::init_model;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            let model_path = app
+                .path()
+                .resource_dir()
+                .expect("[Peppa] resource dir must exist")
+                .join("models/vosk-lgraph-model");
+
+            init_model(model_path);
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![commands::voice::process_audio,])
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("[Peppa] fatal error during startup")
 }
